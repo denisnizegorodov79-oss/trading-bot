9194a626e4099e5b934a72ce4195599f67eb6f3b
denisnizegorodov79-oss <denisnizegorodov79@gmail.com>
2026-06-03 21:40:44 +0300

    Update base.py

--- models/models/base.py
+++ models/models/base.py
@@ -1,9 +1,1 @@
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    """
-    Базовый класс для всех ORM-моделей проекта.
-    """
-
-    pass
+from models.base import Base
