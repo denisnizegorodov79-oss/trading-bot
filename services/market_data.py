cb7af72861c0235181646994b3d4f22945c1a639
denisnizegorodov79-oss <denisnizegorodov79@gmail.com>
2026-06-08 22:44:43 +0300

    Update market_data.py

--- services/market_data.py
+++ services/market_data.py
@@ -1,1 +1,16 @@
+import aiohttp
 
+
+async def get_btc_price() -> float:
+    """
+    Получение текущей цены BTCUSDT с Binance.
+    """
+
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+
+            data = await response.json()
+
+            return float(data["price"])
